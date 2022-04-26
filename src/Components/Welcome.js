80955bc40a75241e477b61f78e4b04a615118a8e
sidtohan <siddhant.tohan@gmail.com>
2022-04-27 00:48:11 +0530

    finished welcome section, animations later

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Welcome = () => {
-  return <section className="welcome"></section>;
+  return (
+    <section className="welcome">
+      <h2 className="welcome-heading">Hello!</h2>
+      <p className="welcome-info">I'm Siddhant Tohan, a fullstack developer</p>
+    </section>
+  );
 };
 
 export default Welcome;
