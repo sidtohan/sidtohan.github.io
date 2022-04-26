d8e013a20e09d70c4d01c21bd49a24ec69042642
sidtohan <siddhant.tohan@gmail.com>
2022-04-27 00:45:59 +0530

    added basic logic for header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import React from "react";
+import Header from "./Components/Header";
+import Welcome from "./Components/Welcome";
+import About from "./Components/About";
+import Skills from "./Components/Skills";
+import Projects from "./Components/Projects";
+import Achievements from "./Components/Achievements";
+import Contact from "./Components/Contact";
 
 const App = () => {
-  return <div></div>;
+  const name = "sidtohan";
+  const sections = [
+    "Home",
+    "About",
+    "Skills",
+    "Projects",
+    "Achievements",
+    "Contact",
+  ];
+
+  return (
+    <>
+      <Header sections={sections} name={name} />
+      <Welcome />
+      <About />
+      <Skills />
+      <Projects />
+      <Achievements />
+      <Contact />
+    </>
+  );
 };
 
 export default App;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,30 @@
 import React from "react";
 
-const Header = ({ name }) => {
-  return <header></header>;
+const Header = ({ name, sections }) => {
+  const handleClick = (section) => {
+    section = section.toLowerCase();
+    if (section === "home") section = "welcome";
+    const reqDiv = document.querySelector(`.${section}`);
+    console.log(reqDiv);
+  };
+  return (
+    <header>
+      <h1 className="header-name">{name}</h1>
+      <nav className="header-nav">
+        <ul>
+          {sections.map((section) => (
+            <li
+              key={section}
+              className="header-nav-option"
+              onClick={() => handleClick(section)}
+            >
+              {section}
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
